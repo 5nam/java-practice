9ae84dbaf797f70d5b910ad3f02680806f4b1560
5nam <ohnam90@gmail.com>
2024-09-09 23:56:16 +0900

    변수 실습

---
+++ java-practice/src/main/java/org/example/section03/variable/Var1.java
@@ -0,0 +1,9 @@
+package org.example.section03.variable;
+
+public class Var1 {
+    public static void main(String[] args) {
+        System.out.println(20); // 변경 10 -> 20
+        System.out.println(20); // 변경 10 -> 20
+        System.out.println(20); // 변경 10 -> 20
+    }
+}
