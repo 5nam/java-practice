e4419af9d648bf81ac86ac063c2d0035f7cf6b04
5nam <ohnam90@gmail.com>
2024-09-10 15:06:43 +0900

    변수 실습

---
+++ java-practice/src/main/java/org/example/section03/variable/VarEx3.java
@@ -0,0 +1,10 @@
+package org.example.section03.variable;
+
+public class VarEx3 {
+    public static void main(String[] args) {
+        long longVar = 10000000000L;
+        System.out.println(longVar);
+        boolean booleanVar = true;
+        System.out.println(booleanVar);
+    }
+}
