61a19f995cfa5a8b44929a85a2688c3f425a88a7
5nam <ohnam90@gmail.com>
2024-09-11 22:38:37 +0900

    조건문 실습

---
+++ java-practice/src/main/java/org/example/section06/CondOp1.java
@@ -0,0 +1,20 @@
+package org.example.section06;
+
+public class CondOp1 {
+    public static void main(String[] args) {
+        // if - else
+        int age = 18;
+        String status;
+        if (age >= 18) {
+            status = "성인";
+        } else {
+            status = "미성년자";
+        }
+        System.out.println("age = " + age + " status = " + status);
+
+        // 삼항 연산자
+        status = (age >= 18) ? "성인" : "미성년자";
+        System.out.println("age = " + age + "status = " + status);
+
+    }
+}
